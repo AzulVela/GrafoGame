9aee5b5ec10479b811a81d8c113cafac2f8e4db9
AzulVela <azul.vela.junio@gmail.com>
2021-07-09 01:59:30 -0300

    Primeros pasos del juego

---
+++ Clases.js
@@ -0,0 +1,97 @@
+
+class Nodo{
+	constructor(x,y){
+		this.x=x;
+		this.y=y;
+		this.color=color(255,255,255);
+		this.arcos=[];
+	}
+	agregarArco(arco){
+		this.arcos.push(arco);
+	}
+	setColor(color){
+		this.color=color;
+	}
+	show(){
+		fill(this.color);
+		ellipse(this.x,this.y,20,20);
+		fill(0);
+	}
+	esNodo(x,y){
+		let enX=(x<= (this.x+10))&&(x>=(this.x-10));
+		let enY=(y<= (this.y+10))&&(y>=(this.y-10))
+		return enX&&enY;
+	}
+	
+	
+}
+class Arco{
+	constructor(ndesde,nhasta){
+		this.ndesde=ndesde;
+		this.nhasta=nhasta;
+		this.nodoSeleccionado;
+	}
+	esArco(nhasta,ndesde){
+		return (this.ndesde.esNodo(ndesde)&&this.nhasta.esNodo(nhasta))||(this.ndesde.esNodo(nhasta)&&this.nhasta.esNodo(ndesde));
+	}
+	show(){
+		this.ndesde.show();
+		this.nhasta.show();
+		stroke(255);
+		strokeWeight(6);
+		line(this.ndesde.x,this.ndesde.y,this.nhasta.x,this.nhasta.y);
+		strokeWeight(1);
+		fill(0);
+	}
+	xInic(x1,y1,x2,y2){
+	}
+	yInic(x1,y1,x2,y2){
+	}
+	xFin(x1,y1,x2,y2){
+	}
+	yFin(x1,y1,x2,y2){
+	}
+	nodoSelec(){
+		return this.nodoSeleccionado;
+	}
+	esNodo(x,y){
+		let isNodoDesde=this.ndesde.esNodo(x,y); 
+		let isNodoHasta=this.nhasta.esNodo(x,y);
+		if(isNodoDesde){
+			this.nodoSeleccionado=this.ndesde;
+		}
+		if(isNodoHasta){
+			this.nodoSeleccionado=this.nhasta;
+		}
+		return isNodoDesde||isNodoHasta;
+	}
+	
+	
+	
+}
+class Grafo{
+	constructor(){
+		this.arcos=[];
+		this.arcosSize=0;
+	}
+	agregarArco(arco){
+		this.arcos.push(arco);
+		this.arcosSize++;
+	}
+	show(){
+		for(let i=0;i<this.arcosSize;i++){
+			this.arcos[i].show();
+		}
+	}
+	pintarNodo(x,y,color){
+		
+		for(let i=0;i<this.arcosSize;i++){
+			if(this.arcos[i].esNodo(x,y)){
+				let nodo=this.arcos[i].nodoSelec();
+				nodo.setColor(color);
+				break;
+			}
+		}
+	}
+	
+}
---
+++ sketch.js
@@ -0,0 +1,46 @@
+let grafo;
+function setup() {
+	createCanvas(400,400);
+	grafo = new Grafo();
+	crearEstrella();
+}
+
+function draw() {
+	background(0);
+	grafo.show();
+}
+function mousePressed(){
+	grafo.pintarNodo(mouseX,mouseY,color(255,0,0));
+}
+function crearEstrella(){
+		let n1=new Nodo(200,10);
+	let n2=new Nodo(230,100);
+	let a12=new Arco(n1,n2);
+	grafo.agregarArco(a12);
+	let n3=new Nodo(390,110);
+	let a23=new Arco(n2,n3);
+	grafo.agregarArco(a23);
+	let n4=new Nodo(300,220);
+	let a34=new Arco(n3,n4);
+	grafo.agregarArco(a34);
+	let n5=new Nodo(350,390);
+	let a45=new Arco(n4,n5);
+	grafo.agregarArco(a45);
+	let n6=new Nodo(200,350);
+	let a56=new Arco(n5,n6);
+	grafo.agregarArco(a56);
+	let n7=new Nodo(50,390);
+	let a67=new Arco(n6,n7);
+	grafo.agregarArco(a67);
+	let n8=new Nodo(100,220);
+	let a78=new Arco(n7,n8);
+	grafo.agregarArco(a78);
+	let n9=new Nodo(10,110);
+	let a89=new Arco(n8,n9);
+	grafo.agregarArco(a89);
+	let n10=new Nodo(170,100);
+	let a910=new Arco(n9,n10);
+	grafo.agregarArco(a910);
+	let a101=new Arco(n10,n1);
+	grafo.agregarArco(a101);
+}
